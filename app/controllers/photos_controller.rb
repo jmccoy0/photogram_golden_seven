d501a53732650e30d950b943a1212838b65ee90a
Jessica McCoy <jmccoy0@users.noreply.github.com>
2017-05-02 18:09:10 -0500

    New photo finished

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,42 @@
+class PhotosController < ApplicationController
+
+def new_form
+  render("photos/new_form.html.erb")
+end
+
+def create_row
+  url = params[:da_source]
+  cap = params[:da_caption]
+  new_photo = Photo.new
+  new_photo.source = url
+  new_photo.caption = cap
+  new_photo.save
+  # render("photos/create_row.html.erb")
+  redirect_to("/photos")
+end
+
+def index
+  @list_of_all_my_photos = Photo.all
+  render("photos/index.html.erb")
+end
+
+def show
+  the_id = params[:da_id]
+  @my_photo = Photo.find(the_id)
+  render("photos/show.html.erb")
+end
+
+def edit_form
+  @the_id = params[:da_id]
+  render("photos/edit_form.html.erb")
+end
+
+def update_row
+  render("photos/update_row.html.erb")
+end
+
+def destroy_row
+  render("photos/destroy_row.html.erb")
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get("/photos/new", {:controller => "photos", :action =>"new_form"})
+  get("/create_photo", {:controller => "photos", :action =>"create_row"})
+  get("/", {:controller => "photos", :action => "index"})
+  get("/photos", {:controller => "photos", :action => "index"})
+  get("/photos/:da_id", {:controller => "photos", :action =>"show"})
+  get("/photos/:id/edit", {:controller => "photos", :action =>"edit_form"})
+  get("/update_photo/:id", {:controller => "photos", :action =>"update_row"})
+  get("/delete_photo/:id", {:controller => "photos", :action =>"destroy_row"})
 end
